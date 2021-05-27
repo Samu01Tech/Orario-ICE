c4ca454b0e42600575d69c3d9d54b22fa21051a4
Samu01Tech <samumaz01@gmail.com>
2021-05-27 13:30:18 +0200

    Update 27/05/2021

--- components/Message.vue
+++ components/Message.vue
@@ -1,28 +1,17 @@
 <template>
   <b-message
-    title="Aggiornamento date Tutorato Probabilità"
+    title="Aggiornamenti"
     type="is-info"
     syze="is-medium"
     has-icon
     aria-close-label="Close message"
   >
     <div class="content">
-      <div class="is-size-6">
-        Buongiorno a tutti, preso atto della poca frequenza nelle sessioni del
-        venerdì, e della fine del corso d'inglese B1 di questa settimana, i
-        prossimi incontri di tutorato saranno i seguenti:
-        <li>
-          Lunedì 24 dalle 14:30 alle 16:30 (non lunedì prossimo! per la prossima
-          settimana si salta)
-        </li>
-        <li>Martedì 25 dalle 11:30 alle 13:30</li>
-        mentre per le settimane successive terremo il lunedì agli stessi orari.
-        Se dovessero servire ulteriori incontri, sono disponibili a fissarne di
-        aggiuntivi.
-        <br>
-        <br>
-        <div class="is-pulled-right is-italic">Matteo Franzil</div>
+      <h3>Prossime date Tutorato Probabilità</h3>
+      <ul>
+        <li>Lunedì 31/5 14:30</li>
+        <li>Lunedì 7/6 14:30</li>
+      </ul>
       </div>
-    </div>
   </b-message>
 </template>
